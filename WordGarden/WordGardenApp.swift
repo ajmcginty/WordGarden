e678d55905d7e82a9a74e48144f4ede646283b01
Aidan McGinty <ajmcginty1@gmail.com>
2026-02-09 12:08:30 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Aidan McGinty on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
